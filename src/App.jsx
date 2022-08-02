31302953bea9f9119d139379084c78336fed8fbd
bhaag-q4 <bhaag.marway@q4inc.com>
2022-08-02 13:24:52 -0400

    Updating routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { Outlet, Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      <h1>A Scrapable Site</h1>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
       >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
+        <Link to="/cats">Cats</Link> |{" "}
+        <Link to="/people">People</Link>
       </nav>
       <Outlet />
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,26 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
-import Invoice from "./routes/invoice";
+import People from "./routes/People";
+import Cats from "./routes/Cats";
+import Cat from "./routes/Cat";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />}>
+        <Route path="people" element={<People />} />
+        <Route path="cats" element={<Cats />}>
           <Route
             index
             element={
               <main style={{ padding: "1rem" }}>
-                <p>Select an invoice</p>
+                <p>Select a cat</p>
               </main>
             }
           />
-          <Route path=":invoiceId" element={<Invoice />} />
+          <Route path=":catId" element={<Cat />} />
         </Route>
         <Route
           path="*"
