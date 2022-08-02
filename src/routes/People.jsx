c12d86fbf75ba411ad4815e5ea69d8db9a8c4700
bhaag-q4 <bhaag.marway@q4inc.com>
2022-08-02 13:25:23 -0400

    Adding people page

---
+++ src/routes/People.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useMemo } from 'react';
+import { index } from "../../db_api.js";
+import Pagination from '../components/pagination';
+
+let PageSize = 3;
+
+export default function People() {
+  const people = index("people");
+  
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return people.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>FIRST NAME</th>
+            <th>LAST NAME</th>
+            <th>EMAIL</th>
+            <th>PHONE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map(item => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td>{item.date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={people.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
+    </>
+  );
+}
