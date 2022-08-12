0e395f1260e19c7f9ac4d64d95ba45243676f940
bhaag-q4 <bhaag.marway@q4inc.com>
2022-08-12 13:32:38 -0400

    broke pagination into multiple steps

---
+++ solutions/pagination_page/solution_1.js
@@ -0,0 +1,16 @@
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch({
+    userDataDir: "./puppeteer_cache", /// caching for speed boost
+    headless: false, // true opens a visible browser
+    slowMo: 200, // slow down in ms
+  });
+  const page = await browser.newPage();
+  await page.goto("http://localhost:1234/", {
+    waitUntil: "networkidle2",
+  });
+
+  // 5. click on nav link to open directory of cats
+  await page.click("nav a:nth-child(2)");
+})();
