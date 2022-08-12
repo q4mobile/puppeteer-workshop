a37f65d3dad1de5533e1865bfaf1b285c1450823
bhaag-q4 <bhaag.marway@q4inc.com>
2022-08-12 13:07:40 -0400

    Full solution for pagination page

--- solutions/solution_2.js
+++ solutions/solution_2.js
@@ -1,46 +1,56 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 (async () => {
   const browser = await puppeteer.launch({
-    userDataDir: './puppeteer_cache', /// caching for speed boost
-    headless: true,
+    userDataDir: "./puppeteer_cache", /// caching for speed boost
+    headless: false,
+    slowmo: 150,
   });
   const page = await browser.newPage();
 
-  await page.goto('http://localhost:1234/', {
-    waitUntil: 'networkidle2'
+  await page.goto("http://localhost:1234/", {
+    waitUntil: "networkidle2",
   });
 
-  await page.click('nav a:nth-child(2)');
+  await page.click("nav a:nth-child(2)");
 
-  const table = await page.waitForSelector('.people_table');
+  await page.waitForSelector(".people_table");
 
-  async function scrapeTable(sel) {
-    const data = await page.evaluate(() => {
-      const rows = Array.from(document.querySelectorAll('table tr'));
-      let tableData = [];
-      rows.forEach((row) => {
-        tableData.push(row.map(td => [{
-          value: td.innerText,
-          class: td.className
-        }]));
-      });
-      return tableData;
-      console.log(data);
+  const scrapeTable = async () => {
+    const currentPageNames = await page.evaluate(() => {
+      const firstNameElements = Array.from(
+        document.querySelectorAll(".person_first-name")
+      );
+      return firstNameElements.map((el) => el.textContent);
     });
+    return currentPageNames;
+  };
+
+  const nextPage = async () => {
+    await page.evaluate(() => {
+      const nextPageButton = document.querySelector(".next_page");
+      nextPageButton.click();
+    });
+  };
+
+  const isNextPageAvailable = async () => {
+    return page.evaluate(() => {
+      const nextPageButton = document.querySelector(".next_page");
+      return !nextPageButton.classList.contains("disabled");
+    });
+  };
+
+  const finalList = [];
+  let nextPageAvailable = true;
+
+  while (nextPageAvailable) {
+    const currentPageNames = await scrapeTable();
+    finalList.push(...currentPageNames);
+    await nextPage();
+    nextPageAvailable = await isNextPageAvailable();
   }
 
-  async function nextPage() {
-    const el = await page.$('.next_page');
-    await el.click('.next_page"]');
-  }
-
-  await scrapeTable('.people_table td');
-
-  // await nextPage();
-  // await nextPage();
-  // await nextPage();
-  // await nextPage();
+  console.log(finalList);
 
   await browser.close();
 })();
