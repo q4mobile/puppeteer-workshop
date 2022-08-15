02e3ed25644b3d0cb96d96546ee117aa17356545
bhaag-q4 <bhaag.marway@q4inc.com>
2022-08-15 10:18:53 -0400

    Emptied out puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -2,21 +2,7 @@ import puppeteer from "puppeteer";
 
 (async () => {
   const browser = await puppeteer.launch({
-    userDataDir: "./puppeteer_cache", /// caching for speed boost
-    headless: false,
-    slowMo: 50,
+    headless: false, // false opens a visible browser
+    slowMo: 200, // slow down in ms
   });
-  const page = await browser.newPage();
-
-  await page.goto("http://localhost:1234/", {
-    waitUntil: "networkidle2",
-  });
-  await page.click("nav a:nth-child(3)");
-
-  await page.type("input#name-input", "Bhaag");
-  await page.type("input#email-input", "Bhaag@bhaagmail.com");
-  await page.click('button[type="submit"');
-  await page.click('button[type="button"');
-
-  await browser.close();
 })();
