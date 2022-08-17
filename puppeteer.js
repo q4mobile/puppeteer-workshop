9cdaa23bdfdbe3f92e2082a64303eba97b1f42ee
Zalmy K <60475531+zkarimi22@users.noreply.github.com>
2022-08-17 10:58:39 -0400

    initial tasks

--- puppeteer.js
+++ puppeteer.js
@@ -3,8 +3,40 @@ import puppeteer from "puppeteer";
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    // slowMo: 150,
+    slowMo: 50,
   });
 
-  browser.close();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:1234/", { waitUntil: "networkidle2" });
+  await page.click('a[href="/cats"]');
+
+  const linkList = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".cat_list-item")).map(
+      (link) => {
+        return link.href;
+      }
+    );
+  });
+  linkList.reverse();
+  await page.goto(linkList[0], { waitUntil: "networkidle2" });
+
+  const firstCatRole = await page.evaluate(() => {
+    return document.querySelector(".cat_item-job_title").textContent;
+  });
+  /* checking our work */
+  console.log(firstCatRole);
+
+  const catRoles = [];
+  for (let i = 0; i < linkList.length; i += 1) {
+    await page.goto(linkList[i], { waitUntil: "networkidle2" });
+
+    const role = await page.evaluate(() => {
+      return document.querySelector(".cat_item-job_title").textContent;
+    });
+    catRoles.push(role);
+}
+console.log(catRoles);
+
+  await page.click('a[href="/people"]');
+  
 })();
